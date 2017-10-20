35deef3117384c74709738b57159ce570132595e
Francis <lara.francis1@gmail.com>
2017-10-20 16:56:18 -0700

    Complete 02_searchable

--- W3D5/active_record_lite/lib/02_searchable.rb
+++ W3D5/active_record_lite/lib/02_searchable.rb
@@ -3,10 +3,26 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = []
+    vals = []
+    params.each do |col, val|
+      where_line << col
+      vals << val
+    end
+    where_line.map! { |val| "#{val} = ?" }
+    result = DBConnection.execute(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line.join(' AND ')}
+    SQL
+
+    parse_all(result)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
