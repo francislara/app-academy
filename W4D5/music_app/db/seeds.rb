1d1725e9b8e43f438066200ec7656f1bcae858d5
Francis <lara.francis1@gmail.com>
2017-10-27 15:28:30 -0700

    Add band views

--- W4D5/music_app/db/seeds.rb
+++ W4D5/music_app/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Band.destroy_all
+
+# User
+fred = User.create!(email: 'fred@metal.com', password: 'password')
+daniel = User.create!(email: 'daniel@classical.com', password: 'password')
+
+# Band
+little_as = Band.create!(name: 'the little \'a\'s')
+big_as = Band.create!(name: 'THE BIG \'A\'s')
